eaaec882736a1abb5d5201905b93d981dc1e729f
Charles Desouza <charles.desouza3000@gmail.com>
2011-10-08 17:17:45 -0400

    diff21 completed...This is too easy :P

---
+++ src/diff21.java
@@ -0,0 +1,24 @@
+/*diff21
+ * Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21. 
+ * diff21(19) → 2
+ * diff21(10) → 11
+ * diff21(21) → 0
+ */
+
+public class diff21 {
+	
+	//Class for testing and setting dummy values
+	public static void main(String [] args ) {	
+		int n = 23;
+		
+		//Output tests
+		System.out.println( diff21(n) );
+	}
+	
+	public static int diff21(int n) {
+		int difference = 21-n;
+		int result = (difference < 0 ) ? (2*Math.abs(difference)) : Math.abs(difference);
+		return result;
+	}
+}
+
