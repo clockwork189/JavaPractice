89fae969b4224b008a03639dd1f5d1444060b1ba
Charles Desouza <charles.desouza3000@gmail.com>
2011-10-15 00:38:19 -0400

    Bonus, find nth fibionacci number

---
+++ src/Fibionacci.java
@@ -0,0 +1,17 @@
+// Back to basics. Fibionacci Series. Oh I miss this one! :P
+public class Fibionacci {
+	public static void main( String[]args ) {
+		// Test value
+		System.out.println(Fibionacci(6));
+	}
+	
+	public static int Fibionacci(int n) {
+		if( n == 0 ) {
+			return 0;
+		} else if( n == 1 ) {
+			return 1;
+		} else {
+			return Fibionacci( n-1 ) + Fibionacci( n -2 );
+		}
+	}
+}
